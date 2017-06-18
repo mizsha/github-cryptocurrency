5957295e41882303d0f3becf9c23cbc1b96cbb3e
Michal Matous <michal.matous@gmail.com>
2017-06-18 14:38:43 +0200

    mizshizace

--- root/cryptocurrency/miners/nheqminer/nheqminer.sh
+++ root/cryptocurrency/miners/nheqminer/nheqminer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tmux new-session -d -s miner '/root/cryptocurrency/miners/nheqminer/nheqminer_16_04 -l equihash.usa.nicehash.com:3357 -u 1egacySQXJA8bLHnFhdQQjZBLW1gxSAjc.worker -cd 0 0'
+./nheqminer_16_04 -l eu.zec.slushpool.com:4444 -u mizsha.ntb -t 2
