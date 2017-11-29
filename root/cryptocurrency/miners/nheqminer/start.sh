4c77f2baf0df2b970a4d85e0f4b4802f919b2e3f
Michal Matouš <michal.matous@gmail.com>
2017-11-29 18:26:33 +0100

    Update start.sh

--- root/cryptocurrency/miners/nheqminer/start.sh
+++ root/cryptocurrency/miners/nheqminer/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/:/usr/lib/nvidia-375
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/:/usr/lib/nvidia-384
 
 /app/github-cryptocurrency/root/cryptocurrency/miners/nheqminer/nheqminer_16_04 -l zec.slushpool.com:4444 -u mizsha.worker001 -cd 0 1 2
